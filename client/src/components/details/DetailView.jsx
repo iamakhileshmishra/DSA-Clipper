ee600331cc0a0376d3f9b6cd5802b53a7ba4da28
iamakhileshmishra <akhileshprince1485@gmail.com>
2023-07-04 22:52:56 +0530

    Text Editor added

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -5,6 +5,8 @@ import { Delete, Edit } from '@mui/icons-material';
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
 import { API } from '../../service/api';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 import { DataContext } from '../../context/DataProvider';
 
@@ -79,30 +81,43 @@ const DetailView = () => {
     }
 
     return (
-        <Container>
-            <Image src={post.picture || url} alt="post" />
-            <Box style={{ float: 'right' }}>
-                {   
-                    account.username === post.username && 
-                    <>  
-                        <Link to={`/update/${post._id}`}><EditIcon color="primary" /></Link>
-                        <DeleteIcon onClick={() => deleteBlog()} color="error" />
-                    </>
-                }
-            </Box>
-            <Heading>{post.title}</Heading>
+      <Container>
+        <Image src={post.picture || url} alt="post" />
+        <Box style={{ float: "right" }}>
+          {account.username === post.username && (
+            <>
+              <Link to={`/update/${post._id}`}>
+                <EditIcon color="primary" />
+              </Link>
+              <DeleteIcon onClick={() => deleteBlog()} color="error" />
+            </>
+          )}
+        </Box>
+        <Heading>{post.title}</Heading>
 
-            <Author>
-                <Link to={`/?username=${post.username}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                    <Typography>Author: <span style={{fontWeight: 600}}>{post.username}</span></Typography>
-                </Link>
-                <Typography style={{marginLeft: 'auto'}}>{new Date(post.createdDate).toDateString()}</Typography>
-            </Author>
+        <Author>
+          <Link
+            to={`/?username=${post.username}`}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <Typography>
+              Author: <span style={{ fontWeight: 600 }}>{post.username}</span>
+            </Typography>
+          </Link>
+          <Typography style={{ marginLeft: "auto" }}>
+            {new Date(post.createdDate).toDateString()}
+          </Typography>
+        </Author>
 
-            <Typography>{post.description}</Typography>
-            <Comments post={post} />
-        </Container>
-    )
+        <ReactQuill
+          value={post.description}
+          readOnly={true}
+          modules={{ toolbar: false }}
+        />
+
+        <Comments post={post} />
+      </Container>
+    );
 }
 
 export default DetailView;
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 
-import { AppBar, Toolbar, styled, Button } from '@mui/material'; 
+import { AppBar, Toolbar, styled, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { useNavigate } from 'react-router-dom';
@@ -38,3 +38,57 @@ const Header = () => {
 }
 
 export default Header;
+// import React, { useState, useEffect } from "react";
+// import { AppBar, Toolbar, styled, Button } from "@mui/material";
+// import { Link } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+
+// const Header = () => {
+//   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+//   const navigate = useNavigate();
+
+//   const toggleTheme = () => {
+//     const newTheme = theme === "light" ? "dark" : "light";
+//     setTheme(newTheme);
+//     localStorage.setItem("theme", newTheme);
+//   };
+
+//   useEffect(() => {
+//     const savedTheme = localStorage.getItem("theme");
+//     if (savedTheme) {
+//       setTheme(savedTheme);
+//     }
+//   }, []);
+
+//   const Component = styled(AppBar)`
+//     background: ${theme === "light" ? "#FFFFFF" : "#000000"};
+//     color: ${theme === "light" ? "black" : "white"};
+//   `;
+
+//   const Container = styled(Toolbar)`
+//     justify-content: center;
+//     & > a {
+//       padding: 20px;
+//       color: ${theme === "light" ? "#000000" : "#FFFFFF"};
+//       text-decoration: none;
+//     }
+//   `;
+
+//   const logout = async () => navigate("/account");
+
+//   return (
+//     <Component>
+//       <Container>
+//         <Link to="/">HOME</Link>
+//         <Link to="/about">ABOUT</Link>
+//         <Link to="/contact">CONTACT</Link>
+//         <Link to="/account">LOGOUT</Link>
+//         <Button onClick={toggleTheme}>
+//           {theme === "light" ? "Dark Theme" : "Light Theme"}
+//         </Button>
+//       </Container>
+//     </Component>
+//   );
+// };
+
+// export default Header;
