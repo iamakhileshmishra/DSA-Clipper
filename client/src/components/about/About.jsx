33d9042bce515a9322c71e1cdd37da797176d9e9
iamakhileshmishra <akhileshprince1485@gmail.com>
2023-07-05 20:03:08 +0530

    About Page updated

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,55 +1,58 @@
-
-import { Box, styled, Typography, Link } from '@mui/material';
-import { GitHub, Instagram, Email } from '@mui/icons-material';
+import { Box, styled, Typography, Link } from "@mui/material";
 
 const Banner = styled(Box)`
-    background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
-    width: 100%;
-    height: 50vh;
-    background-position: left 0px bottom 0px;
-    background-size: cover;
+  background-image: url(https://www.wallpapertip.com/wmimgs/23-236943_us-wallpaper-for-website.jpg);
+  width: 100%;
+  height: 50vh;
+  background-position: left 0px bottom 0px;
+  background-size: cover;
 `;
 
 const Wrapper = styled(Box)`
-    padding: 20px;
-    & > h3, & > h5 {
-        margin-top: 50px;
-    }
+  padding: 20px;
+  & > h3,
+  & > h5 {
+    margin-top: 50px;
+  }
+  margin-bottom: 50px;
 `;
 
 const Text = styled(Typography)`
-    color: #878787;
+  color: #878787;
 `;
 
 const About = () => {
-
-    return (
-        <Box>
-            <Banner/>
-            <Wrapper>
-                <Typography variant="h3">Code for Interview</Typography>
-                <Text variant="h5">I'm a Software Engineer based in India. 
-                    I've built websites, desktop applications and corporate software.<br />
-                    If you are interested, you can view some of my favorite projects here
-                    <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="https://github.com/kunaltyagi9" color="inherit" target="_blank"><GitHub /></Link>
-                    </Box>
-                </Text>
-                <Text variant="h5">
-                    Need something built or simply want to have chat? Reach out to me on
-                    <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="https://www.instagram.com/codeforinterview/" color="inherit" target="_blank">
-                            <Instagram />
-                        </Link>
-                    </Box>  
-                        or send me an Email 
-                        <Link href="mailto:codeforinterview@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
-                            <Email />
-                        </Link>.
-                </Text>
-            </Wrapper>
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Banner />
+      <Wrapper>
+        <Typography variant="h3">DSA Clipper</Typography>
+        <Text variant="h5">
+          Introducing DSA Clipper, a groundbreaking project designed to foster a
+          vibrant community of knowledge-sharing in the realm of Data Structures
+          and Algorithms (DSA). With DSA Clipper, individuals from all walks of
+          life can create their own accounts and contribute their unique DSA
+          shortcuts or algorithms using their preferred approach. This platform
+          serves as a haven for enthusiasts and professionals alike, connecting
+          them with like-minded individuals passionate about DSA. By sharing
+          their valuable insights and experiences, users empower others to learn
+          and grow. The extensive collection of content available on DSA Clipper
+          ensures a diverse range of perspectives and approaches, enabling users
+          to gain a comprehensive understanding of DSA principles.
+          <br />
+          <br />
+          Furthermore, the interactive nature of the platform allows users to
+          engage with the shared content, leaving comments and initiating
+          discussions. Authors retain the freedom to update their posts,
+          guaranteeing that the information remains current and relevant.
+          Embrace the power of collaborative learning and join DSA Clipper today
+          to embark on an enriching journey of knowledge exchange and personal
+          growth.
+          <br />
+        </Text>
+      </Wrapper>
+    </Box>
+  );
+};
 
 export default About;
